97fc63827af21896a293b7234f6f7cb642cdc9bc
Carolina-Gordillo <carolina_gor@hotmail.com>
2023-06-25 12:33:43 -0600

    Se agrego la carpeta

---
+++ Proyecto/Proyecto/Controllers/AlumnoController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto.Model;
+
+namespace Proyecto.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class AlumnoController
+    {
+        public AlumnoController() 
+        {
+        }
+
+        [HttpGet(Name = "GetAlumno")]
+        public List<Alumno> dameLista()
+        {
+            List<Alumno> lista = new List<Alumno>();
+            lista.Add(new Alumno("Carolina", "9", "Java"));
+            lista.Add(new Alumno("Caro", "8", "CSharp"));
+            lista.Add(new Alumno("Lina", "7", "Java"));
+            lista.Add(new Alumno("Joel", "9", "CSharp"));
+            lista.Add(new Alumno("Carlos", "8", "Android"));
+            lista.Add(new Alumno("Gabriel", "7", "Flutter"));
+            return lista;
+        }
+
+        [HttpGet]
+        [Route ("/buscarAlumno")]
+        public dynamic dameAlumno(string nombreAlumno)
+        {
+            Alumno respuesta = null;
+            foreach (Alumno elemento in dameLista()) 
+            {
+                if (elemento.nombre == nombreAlumno)
+                {
+                    respuesta = elemento; break;
+                }
+            }
+
+            if (respuesta != null)
+            {
+                return new
+                {
+                    Resultado = "Alumno encontrado",
+                    Datos = respuesta
+                };
+            }
+            return new
+            {
+                Resultado = "NO SE ENCONTRÓ AL ALUMNO"
+            };
+            
+        }
+
+        //////////
+        //BUSCAR POR CURSO
+        
+         [HttpGet]
+        [Route ("/buscarCurso")]
+        public dynamic dameCurso(string nombreCurso)
+        {
+            List<Alumno> respuesta_curso = new List<Alumno>();
+            foreach (Alumno elemento_curso in dameLista()) 
+            {
+                if (elemento_curso.curso == nombreCurso)
+                {
+                    respuesta_curso.Add((elemento_curso)); //break;                   
+                }
+
+            }
+            
+           if (respuesta_curso != null)
+            {
+                if (!respuesta_curso.Any())
+                {
+                    return new
+                    {
+                        Resultado = "NO SE ENCONTRÓ EL CURSO"
+                    };
+                }
+                return new
+                {
+                    Resultado = "Curso encontrado",
+                    Datos = respuesta_curso
+                };
+            }
+
+           return new
+            {
+                Resultado = "NO SE ENCONTRÓ EL CURSO"
+            };
+            
+        }  
+
+        //////
+
+    }
+}
---
+++ Proyecto/Proyecto/Model/Alumno.cs
@@ -0,0 +1,19 @@
+﻿namespace Proyecto.Model
+{
+    public class Alumno
+    {
+        public string nombre { get; set; }
+        public string calificacion { get; set; }
+        public string curso { get; set; }
+        public Alumno() 
+        {
+        }
+
+        public Alumno(string nombre, string calificacion, string curso)
+        {
+            this.nombre = nombre;
+            this.calificacion = calificacion;
+            this.curso = curso;
+        }
+    }
+}
